0a707a2ff7dc58b0d654119f179f56dbbec13631
Baris Bali <balibaris1@gmail.com>
2021-09-05 15:03:53 +0300

    first commit

---
+++ AutomatedTestDenemeProje1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace AutomatedTestDenemeProje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbSayi1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxSayi1 = new System.Windows.Forms.TextBox();
+            this.tbxSayi2 = new System.Windows.Forms.TextBox();
+            this.btnArti = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.tbxSonuc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbSayi1
+            // 
+            this.lbSayi1.AutoSize = true;
+            this.lbSayi1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbSayi1.Location = new System.Drawing.Point(27, 33);
+            this.lbSayi1.Name = "lbSayi1";
+            this.lbSayi1.Size = new System.Drawing.Size(50, 19);
+            this.lbSayi1.TabIndex = 0;
+            this.lbSayi1.Text = "Sayi1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayi2";
+            // 
+            // tbxSayi1
+            // 
+            this.tbxSayi1.Location = new System.Drawing.Point(83, 35);
+            this.tbxSayi1.Name = "tbxSayi1";
+            this.tbxSayi1.Size = new System.Drawing.Size(100, 20);
+            this.tbxSayi1.TabIndex = 2;
+            // 
+            // tbxSayi2
+            // 
+            this.tbxSayi2.Location = new System.Drawing.Point(83, 70);
+            this.tbxSayi2.Name = "tbxSayi2";
+            this.tbxSayi2.Size = new System.Drawing.Size(100, 20);
+            this.tbxSayi2.TabIndex = 3;
+            // 
+            // btnArti
+            // 
+            this.btnArti.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnArti.Location = new System.Drawing.Point(189, 35);
+            this.btnArti.Name = "btnArti";
+            this.btnArti.Size = new System.Drawing.Size(51, 55);
+            this.btnArti.TabIndex = 4;
+            this.btnArti.Text = "+";
+            this.btnArti.UseVisualStyleBackColor = true;
+            this.btnArti.Click += new System.EventHandler(this.btnArti_Click);
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEksi.Location = new System.Drawing.Point(246, 35);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(51, 55);
+            this.btnEksi.TabIndex = 5;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = true;
+            this.btnEksi.Click += new System.EventHandler(this.btnEksi_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCarp.Location = new System.Drawing.Point(303, 35);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(51, 55);
+            this.btnCarp.TabIndex = 6;
+            this.btnCarp.Text = "*";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBol.Location = new System.Drawing.Point(360, 35);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(51, 55);
+            this.btnBol.TabIndex = 7;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // tbxSonuc
+            // 
+            this.tbxSonuc.Location = new System.Drawing.Point(495, 56);
+            this.tbxSonuc.Name = "tbxSonuc";
+            this.tbxSonuc.Size = new System.Drawing.Size(100, 20);
+            this.tbxSonuc.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(431, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sonuç";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(495, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "green.png");
+            this.ımageList1.Images.SetKeyName(1, "red.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 107);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbxSonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnEksi);
+            this.Controls.Add(this.btnArti);
+            this.Controls.Add(this.tbxSayi2);
+            this.Controls.Add(this.tbxSayi1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbSayi1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxSayi1;
+        private System.Windows.Forms.TextBox tbxSayi2;
+        private System.Windows.Forms.Button btnArti;
+        private System.Windows.Forms.Button btnEksi;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.TextBox tbxSonuc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ AutomatedTestDenemeProje2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomatedTestDenemeProje2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayi1 = 0, sayi2 = 0;
+
+        #region Methods
+        private void initSayi(out int sayi1, out int sayi2)
+        {
+            sayi1 = int.Parse(tbxSayi1.Text.Trim());
+            sayi2 = int.Parse(tbxSayi2.Text.Trim());
+        }
+
+        private int topla(int sayi1, int sayi2)
+        {
+            int result = 0;
+            result = sayi1 + sayi2;
+            return result;
+        }
+
+        private int cikar(int sayi1, int sayi2)
+        {
+            int result = 0;
+            result = sayi1 - sayi2;
+            return result;
+        }
+
+        private int carp(int sayi1, int sayi2)
+        {
+            int result = 0;
+            result = sayi1 * sayi2;
+            return result;
+        }
+
+        private int bol(int sayi1, int sayi2)
+        {
+            int result = 0;
+            result = sayi1 / sayi2;
+            return result;
+        }
+        #endregion
+
+        #region Events
+        private void btnArti_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initSayi(out sayi1, out sayi2);
+                tbxSonuc.Text = (topla(sayi1, sayi2)).ToString();
+                pictureBox1.Image = ımageList1.Images[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }        
+
+        private void btnEksi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initSayi(out sayi1, out sayi2);
+                tbxSonuc.Text = (cikar(sayi1, sayi2)).ToString();
+                pictureBox1.Image = ımageList1.Images[0];//HATA-1
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initSayi(out sayi1, out sayi2);
+                tbxSonuc.Text = (carp(sayi1, sayi2)).ToString();
+                pictureBox1.Image = ımageList1.Images[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initSayi(out sayi1, out sayi2);
+                tbxSonuc.Text = (carp(sayi1, sayi2)).ToString();//HATA-2
+                pictureBox1.Image = ımageList1.Images[1];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+
+    }
+}
